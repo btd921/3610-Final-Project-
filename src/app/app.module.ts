b92fd7ad37c5eced566eb604878ac9fa630eda97
Drew McDonald <mcdonald.679@buckeyemail.osu.edu>
2018-04-12 13:18:05 -0400

    html/css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PlayerComponent } from './player/player.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { AppRouterModule } from './router/router.module';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { AppRouterModule } from './router/router.module';
     PositionsComponent,
     PlayerComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
